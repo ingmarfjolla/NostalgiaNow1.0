2525c3b764d203176f2c67a707fb7f21d60500c7
Ingmar <ingmar@Ingmars-MacBook-Air.local>
2020-10-07 16:04:08 -0400

    Good commit

---
+++ nostalgia-master/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://admin-kareem:Hunter10@cluster0-mkcpg.mongodb.net/nostalgiaDB'
+    // mongoURI: "mongodb+srv://admin-kareem:Hunter10@cluster0-mkcpg.mongodb.net/test?retryWrites=true&w=majority"
+}
